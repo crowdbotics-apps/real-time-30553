79383363fee917384769c9a5084517ea68c28367
Crowdbotics <team@crowdbotics.com>
2021-09-15 01:51:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "real_time_30553Identifier",
+  url: "https://real_time_30553Identifier.botics.co",
+  api: "https://real_time_30553Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
